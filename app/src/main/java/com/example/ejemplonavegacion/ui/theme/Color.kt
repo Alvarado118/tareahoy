4dbf81b07c5444f1fd9e7af7b86ca5713ab1c77c
Anderson Rene <alvazetia@gmail.com>
2023-10-15 21:07:58 -0600

    tarea

---
+++ app/src/main/java/com/example/ejemplonavegacion/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.ejemplonavegacion.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFF44336)
+val PurpleGrey80 = Color(0xFF1A37DA)
+val Pink80 = Color(0xFFD1BE12)
+
+val Purple40 = Color(0xFF2DE00D)
+val PurpleGrey40 = Color(0xFF83E413)
+val Pink40 = Color(0xFFFFC107)
